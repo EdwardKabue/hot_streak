6281501d96da08096f4472b0b22fdcdede3290b0
Edward <kabueedward@yahoo.com>
2015-03-06 22:05:00 +0300

    First commit. Nothing added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HotStreak::Application.config.secret_key_base = 'deaf2c3e66c7f2b40de7ac6f900219032c4bef395dd33fef28567301f82cc27b49a72e9c5ad5c4e1720d153c008adcbd16ddfcece9ab1df6a58a0be8cb5fcc18'
